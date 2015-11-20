dcb0982d5e92743cc799e5e2157d3c703c018783
Kevin Gosse <gossekevin1@gmail.com>
2015-11-20 06:08:15 +0100

    convert scoring, klondike, game and board to modules

--- app/app.js
+++ app/app.js
@@ -1,3 +1,8 @@
+import "klondike/game.js";
+import "klondike/board.js";
+import "klondike/klondike.js";
+import "klondike/scoring.js";
+
 angular.module("solitaire", ["klondike", "ngDraggable"]);
 
 angular.element(document).ready(() => {
