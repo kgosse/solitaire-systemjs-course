d63f9486fc798c5d976263ad592aaba7c928fbd4
Kevin Gosse <gossekevin1@gmail.com>
2015-11-25 06:53:53 +0100

    use amd modules in deck.js

--- app/cards/deck.js
+++ app/cards/deck.js
@@ -1,31 +1,59 @@
 /**
  * Created by kevin gosse on 25/11/2015.
  */
-//import Card from "./cards.js";
-var Card = require("./cards.js").default;
+/*
+ //import Card from "./cards.js";
+ var Card = require("./cards.js").default;
 
-//export default function Deck() {
-function Deck() {
-  this.unShuffled = function unShuffled() {
-    return _.chain(Card.ranksInImagesOrder)
-        .map(function (rank) {
-          return Card.suitsInImagesOrder.map(function (suit) {
-            return {
-              suit: suit,
-              rank: rank
-            };
-          });
-        })
-        .flatten()
-        .map(function (card) {
-          return new Card(card);
-        })
-        .value();
-  };
+ //export default function Deck() {
+ function Deck() {
+ this.unShuffled = function unShuffled() {
+ return _.chain(Card.ranksInImagesOrder)
+ .map(function (rank) {
+ return Card.suitsInImagesOrder.map(function (suit) {
+ return {
+ suit: suit,
+ rank: rank
+ };
+ });
+ })
+ .flatten()
+ .map(function (card) {
+ return new Card(card);
+ })
+ .value();
+ };
 
-  this.shuffled = function shuffled() {
-    return _.shuffle(this.unShuffled());
-  };
-}
+ this.shuffled = function shuffled() {
+ return _.shuffle(this.unShuffled());
+ };
+ }
 
-module.exports = Deck;
+ module.exports = Deck;*/
+
+define(["./cards.js"], function(Card){
+    Card = Card.default;
+    function Deck() {
+        this.unShuffled = function unShuffled() {
+            return _.chain(Card.ranksInImagesOrder)
+                .map(function (rank) {
+                    return Card.suitsInImagesOrder.map(function (suit) {
+                        return {
+                            suit: suit,
+                            rank: rank
+                        };
+                    });
+                })
+                .flatten()
+                .map(function (card) {
+                    return new Card(card);
+                })
+                .value();
+        };
+
+        this.shuffled = function shuffled() {
+            return _.shuffle(this.unShuffled());
+        };
+    }
+    return Deck;
+});
