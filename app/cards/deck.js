66dbe1b0bcf080f895b78bf428db90a236c5b627
Kevin Gosse <gossekevin1@gmail.com>
2015-11-25 06:45:16 +0100

    use commonjs modules in deck.js

--- app/cards/deck.js
+++ app/cards/deck.js
@@ -1,27 +1,31 @@
 /**
  * Created by kevin gosse on 25/11/2015.
  */
-import Card from "./cards.js";
+//import Card from "./cards.js";
+var Card = require("./cards.js").default;
 
-export default function Deck() {
+//export default function Deck() {
+function Deck() {
   this.unShuffled = function unShuffled() {
     return _.chain(Card.ranksInImagesOrder)
-      .map(function (rank) {
-        return Card.suitsInImagesOrder.map(function (suit) {
-          return {
-            suit: suit,
-            rank: rank
-          };
-        });
-      })
-      .flatten()
-      .map(function (card) {
-        return new Card(card);
-      })
-      .value();
+        .map(function (rank) {
+          return Card.suitsInImagesOrder.map(function (suit) {
+            return {
+              suit: suit,
+              rank: rank
+            };
+          });
+        })
+        .flatten()
+        .map(function (card) {
+          return new Card(card);
+        })
+        .value();
   };
 
   this.shuffled = function shuffled() {
     return _.shuffle(this.unShuffled());
   };
 }
+
+module.exports = Deck;
