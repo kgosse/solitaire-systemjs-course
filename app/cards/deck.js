caf700a9610087861681179757ca420248b1650e
Kevin Gosse <gossekevin1@gmail.com>
2015-11-25 06:05:57 +0100

    use global modules

---
+++ app/cards/deck.js
@@ -0,0 +1,27 @@
+/**
+ * Created by kevin gosse on 25/11/2015.
+ */
+import Card from "./cards.js";
+
+export default function Deck() {
+  this.unShuffled = function unShuffled() {
+    return _.chain(Card.ranksInImagesOrder)
+      .map(function (rank) {
+        return Card.suitsInImagesOrder.map(function (suit) {
+          return {
+            suit: suit,
+            rank: rank
+          };
+        });
+      })
+      .flatten()
+      .map(function (card) {
+        return new Card(card);
+      })
+      .value();
+  };
+
+  this.shuffled = function shuffled() {
+    return _.shuffle(this.unShuffled());
+  };
+}
